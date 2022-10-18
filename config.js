72fce4f3cb91a2d8ee6f3334998f99f32881a105
Emagip <72194496+Emagip@users.noreply.github.com>
2022-10-18 10:31:18 +0200

    updated openweather map API key

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: '7d46b1a7dbcabc085b0944210cc500af', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
